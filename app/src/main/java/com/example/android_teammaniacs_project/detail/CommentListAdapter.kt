3e202780593a97960464851fb40333fb9036b67c
yeonghyeon <agilecatch@gmail.com>
2023-09-25 20:13:00 +0900

    [DESIGN] : 🎨 VideoDetailActivity 어댑터 연결
    - 댓글 item 리사이클러뷰 구현완료
    - 댓글 데이터모델 추가
    - 리사이클러뷰 어댑터 디테일 페이지와 연결 완료

---
+++ app/src/main/java/com/example/android_teammaniacs_project/detail/CommentListAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.android_teammaniacs_project.detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_teammaniacs_project.databinding.ComentItemBinding
+
+class CommentListAdapter : RecyclerView.Adapter<CommentListAdapter.ViewHolder>() {
+    private val list = ArrayList<CommentModel>()
+
+    fun addItems(items: ArrayList<CommentModel>) {
+        list.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentListAdapter.ViewHolder {
+        return ViewHolder(
+            ComentItemBinding.inflate(
+                LayoutInflater.from(parent.context)
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item=list[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ViewHolder(
+        private val binding: ComentItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: CommentModel) = with(binding) {
+//            Glide.with(itemView).load(item.url)
+//                .placeholder(R.drawable.loding) // 이미지 로딩 중 사진
+//                .error(R.drawable.no) // 이미지를 불러오지 못했을 때 사진
+//                .into(imageView)
+            tvDate.text = item.date
+            tvComent.text = item.comment
+            tvName.text = item.name
+
+
+        }
+
+    }
+}
+
