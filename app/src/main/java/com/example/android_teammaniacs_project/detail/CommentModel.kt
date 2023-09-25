df4653a8ff4fea92fae1c922b971ddd3541e538a
yeonghyeon <agilecatch@gmail.com>
2023-09-25 20:10:59 +0900

    [DESIGN] : 🎨 VideoDetailActivity
    -ui 작업 완료

---
+++ app/src/main/java/com/example/android_teammaniacs_project/detail/CommentModel.kt
@@ -0,0 +1,9 @@
+package com.example.android_teammaniacs_project.detail
+
+data class CommentModel(
+    val id:Int,
+    val profile:Int,
+    val name:String,
+    val date:String,
+    val comment:String,
+)
