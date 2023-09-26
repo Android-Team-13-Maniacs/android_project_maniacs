c15ab58e850f00631dce791ff9b1514cccc2d972
hyunsik jung <ball1241@naver.com>
2023-09-26 13:57:09 +0900

    [FEATURE] : VideoDetailActivity 공유하기 구현

--- app/src/main/java/com/example/android_teammaniacs_project/detail/VideoDetailActivity.kt
+++ app/src/main/java/com/example/android_teammaniacs_project/detail/VideoDetailActivity.kt
@@ -1,9 +1,12 @@
 package com.example.android_teammaniacs_project.detail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.android_teammaniacs_project.R
 import com.example.android_teammaniacs_project.databinding.VideoDetailActivityBinding
+
 
 class VideoDetailActivity : AppCompatActivity() {
     private lateinit var binding: VideoDetailActivityBinding
@@ -17,6 +20,20 @@ class VideoDetailActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initView()
+
+        val shareButton = findViewById<Button>(com.example.android_teammaniacs_project.R.id.btn_Share)
+
+
+        shareButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "video/*"
+
+            // String으로 받아서 넣기
+            val sendMessage = "이렇게 스트링으로 만들어서 넣어주면 됩니다."
+            intent.putExtra(Intent.EXTRA_TEXT, sendMessage)
+            val shareIntent = Intent.createChooser(intent, "share")
+            startActivity(shareIntent)
+        }
     }
 
     private fun initView() = with(binding) {
@@ -26,8 +43,20 @@ class VideoDetailActivity : AppCompatActivity() {
         //test data
         val list = ArrayList<CommentModel>()
         for ( i in 0..3) {
-            list.add(CommentModel(0, R.drawable.dog,"$i name","$i date","$i coment"))
+            list.add(CommentModel(0, com.example.android_teammaniacs_project.R.drawable.dog,"$i name","$i date","$i coment"))
         }
         recyclerView.addItems(list)
     }
+
+
+    private fun showShareSheet(text: String, imageUri: Uri) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "video/*"
+        intent.putExtra(Intent.EXTRA_TEXT, text)
+        intent.putExtra(Intent.EXTRA_STREAM, imageUri)
+
+        val shareIntent = Intent.createChooser(intent, null)
+        startActivity(shareIntent)
+    }
+
 }
