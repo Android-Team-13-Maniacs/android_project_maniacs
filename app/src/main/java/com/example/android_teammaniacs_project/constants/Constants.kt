80db628f106fb14a0ab2cd1d0e752eda4ad70aa1
minji05 <smj95133@likelion.org>
2023-09-27 22:33:40 +0900

    [FEAT] : ⚙️ | My list 추가 기능

--- app/src/main/java/com/example/android_teammaniacs_project/constants/Constants.kt
+++ app/src/main/java/com/example/android_teammaniacs_project/constants/Constants.kt
@@ -3,5 +3,6 @@ package com.example.android_teammaniacs_project.constants
 class Constants {
     companion object {
         const val GOOGLE_BASE_URL = "https://www.googleapis.com/"
+        const val MY_VIDEOS_KEY = "MY_VIDEOS"
     }
 }
