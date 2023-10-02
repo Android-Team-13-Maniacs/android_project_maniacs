3763856357de38ce575c0073e6afb8437f4b42f9
LeeChoongHwan <choo901@naver.com>
2023-10-02 12:56:48 +0900

    [FIX] : 🔧 무한 스크롤 구현

--- app/src/main/java/com/example/android_teammaniacs_project/constants/GoogleKey.kt
+++ app/src/main/java/com/example/android_teammaniacs_project/constants/GoogleKey.kt
@@ -1,5 +1,9 @@
 package com.example.android_teammaniacs_project.constants
 
 object GoogleKey {
-    const val KEY = "AIzaSyC_IgVDT4omPtNVDigyQdL-k9f4qzXMMBo"
+    const val KEY = "AIzaSyBJapk9W62K1mx0UG_ynUV3dcUqV5wBklc"
+
+    // Second Key "AIzaSyBJapk9W62K1mx0UG_ynUV3dcUqV5wBklc"
+    // First Key "AIzaSyAt61zhZLb8zLb3IFPVCHDpLpYU8oPnWlg"
+
 }
--- app/src/main/java/com/example/android_teammaniacs_project/search/SearchViewModel.kt
+++ app/src/main/java/com/example/android_teammaniacs_project/search/SearchViewModel.kt
@@ -16,26 +16,53 @@ class SearchViewModel(private val apiService: RetrofitInterface) : ViewModel() {
     private val _searchResults = MutableLiveData<List<Video>>()
     val searchResults: LiveData<List<Video>> get() = _searchResults
     var resItems: ArrayList<Video> = ArrayList()
+    var nextPageToken : String? = ""
 
     //검색 API 호출 함수
-    fun searchView(key: String, part: String, maxResults: Int, order: String, q: String?, type: String) {
-        apiService.getSearchList(key,part,maxResults, order, q, type)
+    fun searchVideo(key: String, part: String, maxResults: Int, order: String, q: String?, type: String) {
+        apiService.getSearchList(key,part,maxResults, order, q, type, null)
             ?.enqueue(object : Callback<SearchVideoModel>{
                 override fun onResponse(
                     call: Call<SearchVideoModel>,
                     response: Response<SearchVideoModel>
                 ) {
                     resItems.clear()
+                    nextPageToken = response.body()?.nextPageToken
+                    Log.d("searchToken", nextPageToken.toString())
                     for(i in response.body()?.items!!) {
                         resItems.add(Video(i.snippet.thumbnails.high.url,i.snippet.title,i.snippet.channelId))
                     }
                     _searchResults.value = resItems
+                    Log.d("returnToken", response.body()?.nextPageToken.toString())
                 }
 
                 override fun onFailure(call: Call<SearchVideoModel>, t: Throwable) {
                     Log.d("fail", t.toString())
                 }
 
+            })
+    }
+
+    fun searchVideoScrolled(key : String, part: String, maxResults: Int, order : String, q: String?, type: String) {
+        apiService.getSearchList(key,part,maxResults,order,q,type,nextPageToken)
+            ?.enqueue(object  : Callback<SearchVideoModel>{
+                override fun onResponse(
+                    call: Call<SearchVideoModel>,
+                    response: Response<SearchVideoModel>
+                ) {
+                    resItems.clear()
+                    for (i in response.body()?.items!!) {
+                        resItems.add(Video(i.snippet.thumbnails.high.url, i.snippet.title, i.snippet.channelId))
+                    }
+                    Log.d("nextToken", nextPageToken.toString())
+                    _searchResults.value = resItems
+                    nextPageToken = response.body()?.nextPageToken
+                    Log.d("scrollToken", nextPageToken.toString())
+                }
+
+                override fun onFailure(call: Call<SearchVideoModel>, t: Throwable) {
+
+                }
             })
     }
 }
