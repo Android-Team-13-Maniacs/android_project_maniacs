6e29abbf3655520994a2d68dfb99905d8207dc44
LeeChoongHwan <choo901@naver.com>
2023-10-03 16:45:03 +0900

    [REFACTOR] : ♻️ SearchFragment Refactor

--- app/src/main/java/com/example/android_teammaniacs_project/search/SearchViewModel.kt
+++ app/src/main/java/com/example/android_teammaniacs_project/search/SearchViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.android_teammaniacs_project.search
 
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,14 +11,18 @@ import com.example.android_teammaniacs_project.retrofit.SearchVideoModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.Exception
 
 class SearchViewModel(private val apiService: RetrofitInterface) : ViewModel() {
 
     private val _searchResults = MutableLiveData<List<Video>>()
     val searchResults: LiveData<List<Video>> get() = _searchResults
     var resItems: ArrayList<Video> = ArrayList()
-    var nextPageTokenClass : String? = ""
 
+    //nextPageToken
+    var receivedNextPageToken : String? = ""
+
+    //Scroll 동작을 위한 Boolean Live Data
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> get() = _isLoading
 
@@ -29,18 +34,22 @@ class SearchViewModel(private val apiService: RetrofitInterface) : ViewModel() {
                     call: Call<SearchVideoModel>,
                     response: Response<SearchVideoModel>
                 ) {
-                    resItems.clear()
-                    nextPageTokenClass = response.body()?.nextPageToken
-                    Log.d("searchToken", nextPageTokenClass.toString())
-                    for(i in response.body()?.items!!) {
-                        resItems.add(Video(i.snippet.thumbnails.high.url,i.snippet.title,
-                            i.snippet.channelId,
-                            i.snippet.publishedAt,
-                            i.snippet.channelTitle,
-                            i.snippet.description))
-                    }
-                    _searchResults.value = resItems
-                    Log.d("returnToken", response.body()?.nextPageToken.toString())
+                        resItems.clear()
+                        receivedNextPageToken = response.body()?.nextPageToken
+                        Log.d("searchToken", receivedNextPageToken.toString())
+                        for (i in response.body()?.items!!) {
+                            resItems.add(
+                                Video(
+                                    i.snippet.thumbnails.high.url, i.snippet.title,
+                                    i.snippet.channelId,
+                                    i.snippet.publishedAt,
+                                    i.snippet.channelTitle,
+                                    i.snippet.description
+                                )
+                            )
+                        }
+                        _searchResults.value = resItems
+                        Log.d("returnToken", response.body()?.nextPageToken.toString())
                 }
 
                 override fun onFailure(call: Call<SearchVideoModel>, t: Throwable) {
@@ -51,15 +60,14 @@ class SearchViewModel(private val apiService: RetrofitInterface) : ViewModel() {
     }
 
     fun searchVideoScrolled(key : String, part: String, maxResults: Int, order : String, q: String?, type: String) {
-        apiService.getSearchList(key,part,maxResults,order,q,type,nextPageTokenClass)
+        apiService.getSearchList(key,part,maxResults,order,q,type,receivedNextPageToken)
             ?.enqueue(object  : Callback<SearchVideoModel>{
                 override fun onResponse(
                     call: Call<SearchVideoModel>,
                     response: Response<SearchVideoModel>
                 ) {
-                    resItems.clear()
-                    nextPageTokenClass = response.body()?.nextPageToken
-                    Log.d("scrollToken", nextPageTokenClass.toString())
+                    receivedNextPageToken = response.body()?.nextPageToken
+                    Log.d("scrollToken", receivedNextPageToken.toString())
                     for (i in response.body()?.items!!) {
                         resItems.add(Video(i.snippet.thumbnails.high.url, i.snippet.title,
                             i.snippet.channelId,
@@ -67,7 +75,7 @@ class SearchViewModel(private val apiService: RetrofitInterface) : ViewModel() {
                             i.snippet.channelTitle,
                             i.snippet.description))
                     }
-                    Log.d("nextToken", nextPageTokenClass.toString())
+                    Log.d("nextToken", receivedNextPageToken.toString())
                     _searchResults.value = resItems
                     _isLoading.value = true
                 }
